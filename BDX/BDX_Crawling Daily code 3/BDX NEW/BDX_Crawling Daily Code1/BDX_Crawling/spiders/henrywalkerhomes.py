0347bdc5034d91a5bb412d14bee0cd898da37bb6
Nishant Butani <Nishant.Butani@vinculum.com>
2022-04-29 21:38:09 +0530

    file on 29 april

--- BDX/BDX_Crawling Daily code 3/BDX NEW/BDX_Crawling Daily Code1/BDX_Crawling/spiders/henrywalkerhomes.py
+++ BDX/BDX_Crawling Daily code 3/BDX NEW/BDX_Crawling Daily Code1/BDX_Crawling/spiders/henrywalkerhomes.py
@@ -9,10 +9,95 @@ from BDX_Crawling.items import BdxCrawlingItem_subdivision, BdxCrawlingItem_Plan
 class HickmanhomesSpider(scrapy.Spider):
     name = 'henrywalkerhomes'
     allowed_domains = ['henrywalkerhomes.com']
-    start_urls = ['https://henrywalkerhomes.com/']
+    # start_urls = ['https://henrywalkerhomes.com/']
     builderNumber = '17169'
 
+
+    def start_requests(self):
+        link = 'https://henrywalkerhomes.com/utah/communities/fox-hunter/'
+        yield scrapy.FormRequest(url=link,callback=self.community,dont_filter=True)
+
+    def community(self,response):
+        try:
+            SubdivisionName = response.xpath('//h1/text()').extract_first(default='').strip()
+            print(SubdivisionName)
+        except Exception as e:
+            print(e)
+
+        try:
+            SubdivisionNumber = int(hashlib.md5(bytes(SubdivisionName, "utf8")).hexdigest(), 16) % (10 ** 30)
+        except Exception as e:
+            print(e)
+
+        try:
+            desc=''.join(response.xpath('//h2/../div/div/p/text()').extract()[:1])
+        except:
+            desc=""
+
+        try:
+            images=response.xpath('//div[@class="hover-wrap-inner"]/img/@data-src-webp').extract_first()
+        except:
+            images=''
+
+        try:
+            add = response.xpath('//h1/../div/div/p/text()').extract_first('')
+            street= add.split(",")[0]
+            city = add.split(",")[1].strip()
+            city = city.split(",")[0]
+            state = add.split(",")[2].strip()
+            state = state.split(" ")[0]
+            zip_code = add.split(",")[2].strip()
+            zip_code = zip_code.split(" ")[1]
+        except:
+            pass
+
+        try:
+            a = []
+            aminity = desc
+            amenity_list = ["Pool", "Playground", "GolfCourse", "Tennis", "Soccer", "Volleyball", "Basketball",
+                            "Baseball", "Views", "Lake", "Pond", "Marina", "Beach", "WaterfrontLots", "Park",
+                            "Trails", "Greenbelt", "Clubhouse", "CommunityCenter"]
+            for i in amenity_list:
+                if i in aminity:
+                    a.append(i)
+            ab = '|'.join(a)
+        except Exception as e:
+            print(e)
+
+
+        item = BdxCrawlingItem_subdivision()
+        item['sub_Status'] = "Active"
+        item['SubdivisionNumber'] = SubdivisionNumber
+        item['BuilderNumber'] = self.builderNumber
+        item['SubdivisionName'] = SubdivisionName
+        item['BuildOnYourLot'] = 0
+        item['OutOfCommunity'] = 0
+        item['Street1'] = street
+        item['City'] = city
+        item['State'] = state
+        item['ZIP'] = zip_code
+        item['AreaCode'] = ''
+        item['Prefix'] = ''
+        item['Suffix'] = ''
+        item['Extension'] = ""
+        item['Email'] = 'info@eaglecreekhomes.net'
+        item['SubDescription'] = desc
+        item['SubImage']= images
+        item['SubWebsite'] = response.url
+        item['AmenityType'] = ab
+        yield item
+
+        links = response.xpath('//a[@class="column-link"]/@href').extract()
+        for link in links:
+            link  = 'https://henrywalkerhomes.com/' + link
+            yield scrapy.FormRequest(url=link,callback=self.parse2,dont_filter=True,meta={'SubdivisionNumber':SubdivisionNumber})
+
+        link = 'https://henrywalkerhomes.com/'
+        yield scrapy.FormRequest(url=link,callback=self.parse,dont_filter=True)
+
+
     def parse(self, response):
+
         f = open("html/%s.html" % self.builderNumber, "wb")
         f.write(response.body)
         f.close()
@@ -43,12 +128,18 @@ class HickmanhomesSpider(scrapy.Spider):
 
     def links(self,response):
         links = response.xpath('//div[@class="work-info"]/a/@href').extract()
+        links1 = ['https://henrywalkerhomes.com/the-bryce-grayson-ridge-lot-112-syracuse-ut/']
+        links.extend(links1)
         for link in links:
-            yield scrapy.FormRequest(url=link,callback=self.parse2,dont_filter=True)
+            yield scrapy.FormRequest(url=link,callback=self.parse2,dont_filter=True,meta={'SubdivisionNumber':self.builderNumber})
 
     def parse2(self,response):
+        SubdivisionNumber = response.meta['SubdivisionNumber']
+
         try:
             PlanName = response.xpath('//h2/text()').extract_first()
+            if PlanName == 'Interested In This Home?':
+                PlanName = response.xpath('//h1/text()').extract_first()
         except Exception as e:
             print("PlanName: ", e)
 
@@ -62,10 +153,10 @@ class HickmanhomesSpider(scrapy.Spider):
         except Exception as e:
             print(e)
 
-        try:
-            SubdivisionNumber = self.builderNumber
-        except Exception as e:
-            print(str(e))
+        # try:
+        #     SubdivisionNumber = self.builderNumber
+        # except Exception as e:
+        #     print(str(e))
 
         try:
             PlanNotAvailable = 0
@@ -114,10 +205,14 @@ class HickmanhomesSpider(scrapy.Spider):
             Garage = 0
 
         try:
-            BaseSqft = response.xpath("//*[contains(text(),'Square Feet')]/following-sibling::p/text()").extract_first().strip().replace(',', '')
-            BaseSqft = BaseSqft.split(";")[0]
+            if PlanName == 'The Bryce':
+                BaseSqft = '3650'
+
+            else:
+                BaseSqft = response.xpath("//*[contains(text(),'Square Feet')]/following-sibling::p/text()").extract_first().strip().replace(',', '')
+                BaseSqft = BaseSqft.split(";")[0]
             BaseSqft = ''.join(re.findall(r"(\d+)", BaseSqft))
-            BaseSqft = re.findall(r"(\d+)", BaseSqft)
+            BaseSqft = re.findall(r"(\d+)", BaseSqft)[0]
         except Exception as e:
             BaseSqft = ""
             print("BaseSQFT: ", e)
@@ -144,7 +239,11 @@ class HickmanhomesSpider(scrapy.Spider):
         item['Type'] = Type
         item['PlanNumber'] = PlanNumber
         item['unique_number'] = unique_number  # < -------- Changes here
-        item['SubdivisionNumber'] = SubdivisionNumber
+
+        if PlanName == 'The Auburn':
+            item['SubdivisionNumber'] = '624963386983709107389834044922'
+        else:
+            item['SubdivisionNumber'] = SubdivisionNumber
         item['PlanName'] = PlanName
         item['PlanNotAvailable'] = PlanNotAvailable
         item['PlanTypeName'] = PlanTypeName
--- BDX/BDX_Crawling Daily code 3/BDX NEW/BDX_Crawling Daily Code1/BDX_Crawling/spiders/imagine_homes.py
+++ BDX/BDX_Crawling Daily code 3/BDX NEW/BDX_Crawling Daily Code1/BDX_Crawling/spiders/imagine_homes.py
@@ -9,54 +9,156 @@ from BDX_Crawling.items import BdxCrawlingItem_Builder, BdxCrawlingItem_Corporat
 class ImagineHomesSpider(scrapy.Spider):
     name = 'imagine_homes'
     allowed_domains = ['www.imaginehomessa.com']
-    start_urls = ['http://www.imaginehomessa.com/']
+    # start_urls = ['http://www.imaginehomessa.com/']
 
     builderNumber = 15927
 
-    def parse(self, response):
-        f = open("html/%s.html" % self.builderNumber, "wb")
-        f.write(response.body)
-        f.close()
-        img = response.xpath('//div[@class="ws_images"]/ul/li/img/@src').getall()
-        images = []
-        for i in img:
-            img1 = 'https://www.imaginehomessa.com' + str(i)
-            images.append(img1)
-        images = '|'.join(images)
+    def __init__(self):
+        self.temp_list = []
+
+    def start_requests(self):
+        # def parse(self, response):
+        #     f = open("html/%s.html" % self.builderNumber, "wb")
+        #     f.write(response.body)
+        #     f.close()
+        #     img = response.xpath('//div[@class="ws_images"]/ul/li/img/@src').getall()
+        #     images = []
+        #     for i in img:
+        #         img1 = 'https://www.imaginehomessa.com' + str(i)
+        #         images.append(img1)
+        #     images = '|'.join(images)
+        #
+        #     item = BdxCrawlingItem_subdivision()
+        #     item['sub_Status'] = "Active"
+        #     item['SubdivisionNumber'] = ''
+        #     item['BuilderNumber'] = self.builderNumber
+        #     item['SubdivisionName'] = "No Sub Division"
+        #     item['BuildOnYourLot'] = 0
+        #     item['OutOfCommunity'] = 0
+        #     item['Street1'] = '11467 Huebner Road, Suite 225'
+        #     item['City'] = 'San Antonio'
+        #     item['State'] = 'TX'
+        #     item['ZIP'] = '78230'
+        #     item['AreaCode'] = '210'
+        #     item['Prefix'] = '877'
+        #     item['Suffix'] = '5900'
+        #     item['Extension'] = ""
+        #     item['Email'] = ''
+        #     item['SubDescription'] = '''San Antonio's Imagine Homes is leading the nation in new green building technology. Our national award winning system delivers high performance new homes to San Antonio home buyers that result in savings of over 50% on new home heating and cooling costs.With homes certified by the Energy Star  program, our home owners not only enjoy lower energy costs and increased comfort, but they have peace of mind knowing that our building system helps reduce energy use, water consumption, greenhouse gases, and solid waste generation, while creating a home of value that is ahead of its time. '''
+        #     item['SubImage'] = images
+        #     item['SubWebsite'] = response.url
+        #     item['AmenityType'] = ''
+        #     yield item
+        # url = 'https://www.imaginehomessa.com/floor-plans.php'
+        # yield scrapy.FormRequest(url=url, dont_filter=True, callback=self.Plans_link)
+        #
+        link = 'https://www.imaginehomessa.com/'
+        yield scrapy.FormRequest(url=link,callback=self.community_links,dont_filter=True)
+
+    def community_links(self,response):
+        links = response.xpath("//a[contains(@href,'neighborhoods')]/../following-sibling::li/a/@href").extract()
+        for link in links:
+            link = 'https://www.imaginehomessa.com' + link
+            yield scrapy.FormRequest(url=link,callback=self.community,dont_filter=True)
+
+    def community(self,response):
+        try:
+            SubdivisionName = response.xpath('//h1/text()').extract_first(default='').strip()
+            print(SubdivisionName)
+        except Exception as e:
+            print(e)
+
+        try:
+            SubdivisionNumber = int(hashlib.md5(bytes(SubdivisionName, "utf8")).hexdigest(), 16) % (10 ** 30)
+        except Exception as e:
+            print(e)
+
+        try:
+            desc=''.join(response.xpath('//h1/../p[2]/text()').extract())
+        except:
+            desc=""
+
+        try:
+            images=response.xpath('//a/img[@alt="Header Image"]/@src').extract_first()
+        except:
+            images=''
+
+        try:
+            if SubdivisionName == "Highland Estates":
+                add = '28917 Windlesham Way, San Antonio, TX 78260'
+            elif SubdivisionName == "Palacios in Cibolo Canyons":
+                add = "4017 Abasolo, San Antonio, TX 78261"
+            elif SubdivisionName == "Sunday Creek at Kinder Ranch":
+                add = "28917 Windlesham Way, San Antonio, TX 78260"
+            else:
+                add = "311 E Evergreen St, San Antonio, TX 78212"
+
+            # add = response.xpath('//h1/../div/div/p/text()').extract_first('')
+            street= add.split(",")[0]
+            city = add.split(",")[1].strip()
+            city = city.split(",")[0]
+            state = add.split(",")[2].strip()
+            state = state.split(" ")[0]
+            zip_code = add.split(",")[2].strip()
+            zip_code = zip_code.split(" ")[1]
+        except:
+            pass
+
+        try:
+            a = []
+            aminity = response.text
+            amenity_list = ["Pool", "Playground", "GolfCourse", "Tennis", "Soccer", "Volleyball", "Basketball",
+                            "Baseball", "Views", "Lake", "Pond", "Marina", "Beach", "WaterfrontLots", "Park",
+                            "Trails", "Greenbelt", "Clubhouse", "CommunityCenter"]
+            for i in amenity_list:
+                if i in aminity:
+                    a.append(i)
+            ab = '|'.join(a)
+        except Exception as e:
+            print(e)
+
 
         item = BdxCrawlingItem_subdivision()
         item['sub_Status'] = "Active"
-        item['SubdivisionNumber'] = ''
+        item['SubdivisionNumber'] = SubdivisionNumber
         item['BuilderNumber'] = self.builderNumber
-        item['SubdivisionName'] = "No Sub Division"
+        item['SubdivisionName'] = SubdivisionName
         item['BuildOnYourLot'] = 0
         item['OutOfCommunity'] = 0
-        item['Street1'] = '11467 Huebner Road, Suite 225'
-        item['City'] = 'San Antonio'
-        item['State'] = 'TX'
-        item['ZIP'] = '78230'
-        item['AreaCode'] = '210'
-        item['Prefix'] = '877'
-        item['Suffix'] = '5900'
+        item['Street1'] = street
+        item['City'] = city
+        item['State'] = state
+        item['ZIP'] = zip_code
+        item['AreaCode'] = ''
+        item['Prefix'] = ''
+        item['Suffix'] = ''
         item['Extension'] = ""
-        item['Email'] = ''
-        item['SubDescription'] = '''San Antonio's Imagine Homes is leading the nation in new green building technology. Our national award winning system delivers high performance new homes to San Antonio home buyers that result in savings of over 50% on new home heating and cooling costs.With homes certified by the Energy Star  program, our home owners not only enjoy lower energy costs and increased comfort, but they have peace of mind knowing that our building system helps reduce energy use, water consumption, greenhouse gases, and solid waste generation, while creating a home of value that is ahead of its time. '''
-        item['SubImage'] = images
+        item['Email'] = 'info@eaglecreekhomes.net'
+        item['SubDescription'] = desc
+        item['SubImage']= images
         item['SubWebsite'] = response.url
-        item['AmenityType'] = ''
+        item['AmenityType'] = ab
         yield item
 
-        url = 'https://www.imaginehomessa.com/floor-plans.php'
-        yield scrapy.FormRequest(url=url, dont_filter=True, callback=self.Plans_link)
-    #
+        temp_dict = {'subdivisonNumber': SubdivisionNumber, 'SubdivisionName': SubdivisionName}
+        self.temp_list.append(temp_dict)
+
+        link = 'https://www.imaginehomessa.com/floor-plans.php'
+        yield scrapy.FormRequest(url=link,callback=self.Plans_link,dont_filter=True)
+
     def Plans_link(self, response):
+
         urls = response.xpath('//tr[@valign="top"]/td[1]/a/@href').extract()
         name = response.xpath('//tr[@valign="top"]/td[1]/a/text()').extract()
         sqft = response.xpath('//tr[@valign="top"]/td[3]/text()').getall()
         bed = response.xpath('//tr[@valign="top"]/td[4]/text()').getall()
         bath = response.xpath('//tr[@valign="top"]/td[5]/text()').getall()
         garage = response.xpath('//tr[@valign="top"]/td[8]/text()').getall()
-        for i,name,sqft,bed,bath,garage in zip(urls,name,sqft,bed,bath,garage):
+        try:
+            comm_name = response.xpath('//tr[@valign="top"]/td[9]/a/text()').getall()
+        except Exception as e:
+            print(e)
+        for i,name,sqft,bed,bath,garage,comm_name in zip(urls,name,sqft,bed,bath,garage,comm_name):
             url = 'https://www.imaginehomessa.com/' + str(i)
             PN = name
             PSF = sqft
@@ -64,28 +166,19 @@ class ImagineHomesSpider(scrapy.Spider):
             PBTH = bath
             PG = garage
             print('PLANS------------------->', url)
-            yield scrapy.FormRequest(url=url, dont_filter=True, callback=self.Plans_Details,meta={'PN':PN,'PSF':PSF,'PB':PB,'PBTH':PBTH,'PG':PG})
+            yield scrapy.FormRequest(url=url, dont_filter=True, callback=self.Plans_Details,
+                                     meta={'PN':PN,'PSF':PSF,'PB':PB,'PBTH':PBTH,'PG':PG,'comm_name':comm_name})
     #
     def Plans_Details(self, response):
-
+        comm_name = response.meta['comm_name']
         Type = 'SingleFamily'
-
         PlanName = response.meta['PN']
-
         PlanNumber = int(hashlib.md5(bytes(PlanName, "utf8")).hexdigest(), 16) % (10 ** 30)
-
         SubdivisionNumber = self.builderNumber
-
         PlanNotAvailable = 0
-
         PlanTypeName = 'Single Family'
-
-
         PlanWebsite = response.url
-
-
         Bedrooms = response.meta['PB']
-
         a = response.meta['PBTH']
         Baths = str(a).split('.')[0]
         HB = str(a).split('.')[-1]
@@ -93,16 +186,13 @@ class ImagineHomesSpider(scrapy.Spider):
             HalfBaths = 0
         else:
             HalfBaths = 1
-
         Garage = response.meta['PG']
-
         BaseSqft = response.meta['PSF']
-
         try:
-            Description = '''With homes certified by the Energy Star  program, our home owners not only enjoy lower energy costs and increased comfort, but they have peace of mind knowing that our building system helps reduce energy use, water consumption, greenhouse gases, and solid waste generation, while creating a home of value that is ahead of its time. Start Your Home Building Experience with Imagine Homes!Imagine Homes has a rich history of San Antonio new home building experience. From the sales team to your personal builder, we strive to make you a raving fan through the home building process and beyond.'''
+            # Description = '''With homes certified by the Energy Star  program, our home owners not only enjoy lower energy costs and increased comfort, but they have peace of mind knowing that our building system helps reduce energy use, water consumption, greenhouse gases, and solid waste generation, while creating a home of value that is ahead of its time. Start Your Home Building Experience with Imagine Homes!Imagine Homes has a rich history of San Antonio new home building experience. From the sales team to your personal builder, we strive to make you a raving fan through the home building process and beyond.'''
+            Description = ''
         except:
             Description = ''
-
 
         try:
             img = response.xpath('//p[@class="floorplan"]/img/@src').get()
@@ -111,26 +201,32 @@ class ImagineHomesSpider(scrapy.Spider):
             ElevationImage = ''
             print(str(e))
 
-        unique = str(PlanNumber) + str(SubdivisionNumber)  # < -------- Changes here
-        unique_number = int(hashlib.md5(bytes(unique, "utf8")).hexdigest(), 16) % (10 ** 30)  # < -------- Changes here
-        item = BdxCrawlingItem_Plan()
-        item['Type'] = Type
-        item['PlanNumber'] = PlanNumber
-        item['unique_number'] = unique_number  # < -------- Changes here
-        item['SubdivisionNumber'] = SubdivisionNumber
-        item['PlanName'] = PlanName
-        item['PlanNotAvailable'] = PlanNotAvailable
-        item['PlanTypeName'] = PlanTypeName
-        item['BasePrice'] = 0
-        item['BaseSqft'] = BaseSqft
-        item['Baths'] = Baths
-        item['HalfBaths'] = HalfBaths
-        item['Bedrooms'] = Bedrooms
-        item['Garage'] = Garage
-        item['Description'] = Description
-        item['ElevationImage'] = ElevationImage
-        item['PlanWebsite'] = PlanWebsite
-        yield item
+        for i in self.temp_list:
+
+            subdivisionnumber = i['subdivisonNumber']
+            SubdivisionName = i['SubdivisionName']
+            if comm_name == SubdivisionName:
+
+                unique = str(PlanNumber) + str(SubdivisionNumber)  # < -------- Changes here
+                unique_number = int(hashlib.md5(bytes(unique, "utf8")).hexdigest(), 16) % (10 ** 30)  # < -------- Changes here
+                item = BdxCrawlingItem_Plan()
+                item['Type'] = Type
+                item['PlanNumber'] = PlanNumber
+                item['unique_number'] = unique_number  # < -------- Changes here
+                item['SubdivisionNumber'] = subdivisionnumber
+                item['PlanName'] = PlanName
+                item['PlanNotAvailable'] = PlanNotAvailable
+                item['PlanTypeName'] = PlanTypeName
+                item['BasePrice'] = 0
+                item['BaseSqft'] = BaseSqft
+                item['Baths'] = Baths
+                item['HalfBaths'] = HalfBaths
+                item['Bedrooms'] = Bedrooms
+                item['Garage'] = Garage
+                item['Description'] = Description
+                item['ElevationImage'] = ElevationImage
+                item['PlanWebsite'] = PlanWebsite
+                yield item
 
 if __name__ == '__main__':
     from scrapy.cmdline import execute
